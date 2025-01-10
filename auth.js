cbca02d4dc3df80212d4d5cc928ed294733c3970
Tejas Tarse <tarsetejas@gmail.com>
2025-01-10 11:54:13 +0530

    changes in auth.js file

--- auth.js
+++ auth.js
@@ -1,26 +1,26 @@
-
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const Person = require('./models/Person.js');
 
-passport.use(new LocalStrategy(async (username,password,done)=>{
-  try{
-    const user = await Person.findOne({username:username});
+passport.use(new LocalStrategy(async (username, password, done) => {
+  try {
+    // Find user by username
+    const user = await Person.findOne({ username: username });
 
-    if(!user){
-      return done(null,false,{message:'User not found'});
+    if (!user) {
+      return done(null, false, { message: 'User not found' });
     }
-    const ispsswordValid = user.comparePassword(password);
-    if(ispsswordValid){
-      return done(null,user );
-    }else{
-      return done(null,false,{message:'Password is incorrect'});
+
+    // Compare password
+    const isPasswordValid = await user.comparePassword(password);
+    if (isPasswordValid) {
+      return done(null, user);
+    } else {
+      return done(null, false, { message: 'Password is incorrect' });
     }
-  }catch(err){
+  } catch (err) {
     return done(err);
   }
-}))
-
-// app.use(logRequest);
+}));
 
 module.exports = passport;
